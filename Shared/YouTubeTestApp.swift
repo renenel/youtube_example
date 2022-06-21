fd93ab456f7114a0ef986c9013073db6196ab0a7
Renen Avneri <renen.avneri@getnexar.com>
2022-06-21 12:01:18 +0300

    Initial Commit

---
+++ Shared/YouTubeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeTestApp.swift
+//  Shared
+//
+//  Created by Renen Avneri on 21/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
